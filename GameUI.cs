62ec254652e96e81230c082a97cf14b7dd7e4954
pepinovimr <pepino.vimr@seznam.cz>
2022-11-25 18:31:09 +0100

    Add project files.

---
+++ GameUI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Platforms
+{
+    public partial class GameUI : Form
+    {
+        public GameUI()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true; //redraws into second buffer, prevents filcker
+        }
+    }
+}
